81a700a928343b815c55f1da788a03e2174c6d8b
Jameka Echols <jameka.echols@gmail.com>
2019-02-28 14:25:51 -0600

    added a for loop in the if statement to add the multiplied symbols

--- overly-excited.js
+++ overly-excited.js
@@ -22,7 +22,103 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
 
 // The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one 
 // operation when a certain conditions is true, and a different operation if that condition is false.
-function addExcitement (theWordArray) {
+// function addExcitement (theWordArray) {
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         /*
+//             If the current value of `i` divided by 3 has no
+//             remainder, add an exclamation point to the end of
+//             the word and then concatenate it to `buildMeUp`.
+        
+//             Otherwise, just concatenate the word itself.
+//          */
+//         // Print buildMeUp to the console
+//         if((i + 1) % 3 === 0){
+//             buildMeUp += theWordArray[i] + "! "
+//         }else {
+//             buildMeUp += theWordArray[i] + " "
+//         }
+//         console.log(buildMeUp)
+//     }
+// }
+
+// The learning objective of this exercise is to practice defining and using function arguments.
+
+// Add a new argument to the function so that a developer can specify which character should be 
+// displayed instead of it always being an exclamation point.
+
+// function addExcitement (theWordArray) {
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         /*
+//             If the current value of `i` divided by 3 has no
+//             remainder, add an exclamation point to the end of
+//             the word and then concatenate it to `buildMeUp`.
+        
+//             Otherwise, just concatenate the word itself.
+//             */
+//         // Print buildMeUp to the console
+//         if((i + 1) % 3 === 0){
+//             buildMeUp += theWordArray[i] + "! "
+//         }else {
+//             buildMeUp += theWordArray[i] + " "
+//         }
+//         console.log(buildMeUp)
+//     }
+// }
+
+// function addExcitement (theWordArray, symbol) {
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         /*
+//             If the current value of `i` divided by 3 has no
+//             remainder, add an exclamation point to the end of
+//             the word and then concatenate it to `buildMeUp`.
+        
+//             Otherwise, just concatenate the word itself.
+//             */
+//         // Print buildMeUp to the console
+//         if((i + 1) % 3 === 0){
+//             buildMeUp += theWordArray[i] + symbol + " "
+//         }else {
+//             buildMeUp += theWordArray[i] + " "
+//         }
+//         console.log(buildMeUp)
+//     }
+// }
+
+
+// Add a new argument to the function so that a developer 
+// can specify how many times the special character should be added.
+// function addExcitement (theWordArray, symbol, multiplier) {
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         /*
+//             If the current value of `i` divided by 3 has no
+//             remainder, add an exclamation point to the end of
+//             the word and then concatenate it to `buildMeUp`.
+        
+//             Otherwise, just concatenate the word itself.
+//             */
+//         // Print buildMeUp to the console
+//         if((i + 1) % 3 === 0){
+//             buildMeUp += theWordArray[i] 
+//             for(let i = 0; i < multiplier; i++){
+//                 buildMeUp += symbol
+//             }
+//             buildMeUp += " "
+//         }else {
+//             buildMeUp += theWordArray[i] + " "
+//         }
+//         console.log(buildMeUp)
+//     }
+// }
+
+const addExcitement = (theWordArray, symbol, multiplier)  => {
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
@@ -32,10 +128,14 @@ function addExcitement (theWordArray) {
             the word and then concatenate it to `buildMeUp`.
         
             Otherwise, just concatenate the word itself.
-         */
+            */
         // Print buildMeUp to the console
-        if(i % 3 === 0){
-            buildMeUp += theWordArray[i] + "! "
+        if((i + 1) % 3 === 0){
+            buildMeUp += theWordArray[i] 
+            for(let i = 0; i < multiplier; i++){
+                buildMeUp += symbol
+            }
+            buildMeUp += " "
         }else {
             buildMeUp += theWordArray[i] + " "
         }
@@ -43,9 +143,8 @@ function addExcitement (theWordArray) {
     }
 }
 
-
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence, "?", 4);
 
 
 
